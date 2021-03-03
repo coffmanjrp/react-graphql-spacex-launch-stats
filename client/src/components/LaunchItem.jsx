b43f14487189256dac995dce5e93ed4b3cd48b7c
coffmanjrp <coffmanjrp@gmail.com>
2021-03-03 00:07:33 -0800

    Create MissionKey component and className condition to LaunchItem

--- client/src/components/LaunchItem.jsx
+++ client/src/components/LaunchItem.jsx
@@ -1,3 +1,5 @@
+import classNames from 'classnames';
+
 const LaunchItem = ({
   launch: { flight_number, launch_date_local, launch_success, mission_name },
 }) => {
@@ -6,7 +8,17 @@ const LaunchItem = ({
       <div className="card card-body mb-3">
         <div className="row">
           <div className="col-md-9">
-            <h4>Mission: {mission_name}</h4>
+            <h4>
+              Mission:{' '}
+              <span
+                className={classNames({
+                  'text-success': launch_success,
+                  'text-danger': !launch_success,
+                })}
+              >
+                {mission_name}
+              </span>
+            </h4>
             <p>Data: {launch_date_local}</p>
           </div>
           <div className="col-md-3">
--- client/src/components/Launches.jsx
+++ client/src/components/Launches.jsx
@@ -1,5 +1,6 @@
 import { useQuery, gql } from '@apollo/client';
 import LaunchItem from './LaunchItem';
+import MissionKey from './MissionKey';
 
 const LAUNCHES_QUERY = gql`
   query LauncesQuery {
@@ -26,6 +27,7 @@ const Launches = () => {
   return (
     <>
       <h1 className="display-4 my-3">Launches</h1>
+      <MissionKey />
       {data.launches.map((launch) => (
         <LaunchItem key={launch.flight_number} launch={launch} />
       ))}

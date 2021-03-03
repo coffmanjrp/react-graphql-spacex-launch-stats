252a715b0bfee61a8fbf785a65d9df6a98b441f8
coffmanjrp <coffmanjrp@gmail.com>
2021-03-03 02:59:04 -0800

    Fix key error issues

--- client/src/components/Launches.jsx
+++ client/src/components/Launches.jsx
@@ -28,8 +28,8 @@ const Launches = () => {
     <>
       <h1 className="display-4 my-3">Launches</h1>
       <MissionKey />
-      {data.launches.map((launch) => (
-        <LaunchItem key={launch.flight_number} launch={launch} />
+      {data.launches.map((launch, index) => (
+        <LaunchItem key={index} launch={launch} />
       ))}
     </>
   );

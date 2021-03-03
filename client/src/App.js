6366da6b2fe2d2646477f1defb908cc84015b0cd
coffmanjrp <coffmanjrp@gmail.com>
2021-03-02 22:57:49 -0800

    Create react app and clean it up

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import logo from './logo.png';
+
+function App() {
+  return (
+    <div>
+      <img
+        src={logo}
+        alt={'SpaceX'}
+        style={{ width: 300, display: 'block', margin: 'auto' }}
+      />
+    </div>
+  );
+}
+
+export default App;

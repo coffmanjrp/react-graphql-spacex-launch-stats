24ebe52a7b85868c1c9ed1e03a11501b5522b7c9
coffmanjrp <coffmanjrp@gmail.com>
2021-03-03 02:57:59 -0800

    distructure variables

--- client/src/components/Launch.jsx
+++ client/src/components/Launch.jsx
@@ -33,41 +33,40 @@ const Launch = ({ match: { params } }) => {
     console.log(error);
   }
 
+  const {
+    mission_name,
+    launch_year,
+    launch_success,
+    rocket: { rocket_id, rocket_name, rocket_type },
+  } = data.launch;
+
   return (
     <>
       <div>
         <h1 className="display-4 my-3">
-          <span className="text-dark">Mission:</span> {data.launch.mission_name}
+          <span className="text-dark">Mission:</span> {mission_name}
         </h1>
         <h4 className="md-3">Launch Details</h4>
         <ul className="list-group">
           <li className="list-group-item">Flight Number: {flight_number}</li>
-          <li className="list-group-item">
-            Launch Year: {data.launch.launch_year}
-          </li>
+          <li className="list-group-item">Launch Year: {launch_year}</li>
           <li className="list-group-item">
             Launch Successful:{' '}
             <span
               className={classNames({
-                'text-success': data.launch.launch_success,
-                'text-danger': !data.launch.launch_success,
+                'text-success': launch_success,
+                'text-danger': !launch_success,
               })}
             >
-              {data.launch.launch_success ? 'Yes' : 'No'}
+              {launch_success ? 'Yes' : 'No'}
             </span>
           </li>
         </ul>
         <h4 className="my-3">Rocket Details</h4>
         <ul className="list-group">
-          <li className="list-group-item">
-            Rocket ID: {data.launch.rocket.rocket_id}
-          </li>
-          <li className="list-group-item">
-            Rocket Name: {data.launch.rocket.rocket_name}
-          </li>
-          <li className="list-group-item">
-            Rocket Type: {data.launch.rocket.rocket_type}
-          </li>
+          <li className="list-group-item">Rocket ID: {rocket_id}</li>
+          <li className="list-group-item">Rocket Name: {rocket_name}</li>
+          <li className="list-group-item">Rocket Type: {rocket_type}</li>
         </ul>
         <hr />
         <Link to="/" className="btn btn-secondary">
